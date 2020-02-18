4b682cc8cbdee64f626d77e2f1c9f2f76757e689
David Andersson <david.andersson@zocom.se>
2020-02-18 16:09:30 +0100

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Day } from './day';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calendar';
+// function f(x: number, y: string): void {}
+export class AppComponent implements OnInit {
+	title = 'calendar';
+	today = Date.now();
+	days: Day[] = [
+		{
+			day: '2020-02-17',
+			content: 'Pluggade Angular'
+		},
+		{
+			day: '2020-02-18',
+			content: 'Bästa lektionen EVER'
+		},
+		{
+			day: '2020-02-19',
+			content: 'Repetera'
+		}
+	]
+
+	ngOnInit(): void {
+		console.log('days is:', this.days)
+	}
 }
+
+
+
+
+
+
+
+
+
+//
