859ad4f797d8a5a0ec435d4b6e0591e6f97d7ca3
David Andersson <david.andersson@zocom.se>
2020-02-20 11:32:42 +0100

    Added comments

--- src/app/day-info/day-info.component.ts
+++ src/app/day-info/day-info.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
 	styleUrls: ['./day-info.component.css']
 })
 export class DayInfoComponent implements OnInit {
+	// @Input() talar om att våra class properties day och content förväntar sig ett värde från parent component.
+	// OBS! Kom ihåg att importera Input från @angular/core!
 	@Input() day: string = '2020-02-20';
 	@Input() content: string = 'Lunch';
 
